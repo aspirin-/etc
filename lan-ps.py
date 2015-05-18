5e23625b4046c19112e0466961d55f39ef1a993d
aspirin <sethll@users.noreply.github.com>
2015-05-18 00:44:34 -0500

    defined get_base_addresses(); rudimentary error-handling

--- lan-ps.py
+++ lan-ps.py
@@ -9,4 +9,21 @@ import sys
 from netifaces import interfaces, ifaddresses, AF_INET
 from scapy.all import send, ICMP, IP 
 
-send(IP(dst="172.16.8.1")/ICMP())
+def get_base_addresses(): 
+    base_addresses = []
+    for iface in interfaces(): 
+        inet_info = {}
+        if AF_INET in ifaddresses(iface):
+            inet_info = ifaddresses(iface)[AF_INET][0]
+        if 'broadcast' in inet_info:
+            base_addresses.append(inet_info['addr'])
+    return base_addresses
+
+
+base_addresses = get_base_addresses()
+print base_addresses
+
+try: 
+    send(IP(dst="172.16.8.1")/ICMP())
+except Exception, the_exception: 
+    print the_exception

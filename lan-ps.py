628af8a0df3aab50cbf016103c8561c218fbb524
aspirin <sethll@users.noreply.github.com>
2015-05-17 23:48:53 -0500

    added netifaces (pip)

--- lan-ps.py
+++ lan-ps.py
@@ -1,7 +1,12 @@
 #!/usr/env/python
+#
 # Note: root is required to send ICMP packets
+#
+# Dependencies: 
+#   netifaces (pip install netifaces) 
 
 import sys
+from netifaces import interfaces, ifaddresses, AF_INET
 from scapy.all import send, ICMP, IP 
 
 send(IP(dst="172.16.8.1")/ICMP())

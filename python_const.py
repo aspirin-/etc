94faaf32ee5840bff8e390dadb9a9ba443c1b207
(none) <(none)>
2016-03-18 01:24:36 -0500

    python constant snippet

---
+++ python_const.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+
+""" Snippet for creating 'constants' in python
+    http://stackoverflow.com/a/2688086
+"""
+def constant(f):
+    def fset(self, value):
+        raise TypeError
+    def fget(self):
+        return f()
+    return property(fget, fset)
+
+class _Const(object):
+    @constant
+    def FOO():
+        return 0xBAADFACE
+    @constant
+    def BAR():
+        return 0xDEADBEEF
+
+CONST = _Const()

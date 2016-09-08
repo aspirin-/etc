38a578247c41f615886d83d08dd20723a042a406
sethll <seth.leick@gmail.com>
2016-09-08 13:13:15 -0500

    unknown change

--- python_const.py
+++ python_const.py
@@ -3,17 +3,22 @@
 """ Snippet for creating 'constants' in python
     http://stackoverflow.com/a/2688086
 """
+
+
 def constant(f):
     def fset(self, value):
         raise TypeError
+
     def fget(self):
         return f()
     return property(fget, fset)
+
 
 class _Const(object):
     @constant
     def FOO():
         return 0xBAADFACE
+
     @constant
     def BAR():
         return 0xDEADBEEF

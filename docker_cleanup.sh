e0b8db10dd5c4799a14487ccda5105471a107d95
Seth Leick <sethll@users.noreply.github.com>
2017-04-14 16:09:44 -0700

    docker_cleanup.sh executable, for bash

--- docker_cleanup.sh
+++ docker_cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e 
 
-docker rm (docker ps -aq)
+docker rm $(docker ps -aq)
 docker images | grep none | awk '{print "docker rmi " $3;}' | sh
 docker volume ls -qf dangling=true | xargs -r docker volume rm

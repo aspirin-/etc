7d681999c37950e8479873a7342eaeeb730d7fbb
Seth Leick <sethll@users.noreply.github.com>
2017-04-14 16:07:04 -0700

    this is bash not fish

--- docker_cleanup.sh
+++ docker_cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e 
 
-docker rm (docker ps -aq)
-docker images | grep none | awk '{print "docker rmi " $3;}' | sh
-docker volume ls -qf dangling=true | xargs -r docker volume rm
+sudo docker rm (sudo docker ps -aq)
+sudo docker images | grep none | awk '{print "sudo docker rmi " $3;}' | sh
+sudo docker volume ls -qf dangling=true | xargs -r sudo docker volume rm
